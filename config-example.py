126e84064df628665a0ac657485b0ce3ffacaec7
Guilherme Vahl <guilherme.vahl@nave.rs>
2020-08-16 18:02:10 -0300

    feat: config-example initial values

---
+++ config-example.py
@@ -0,0 +1,12 @@
+username="YOUR_LOGIN_HERE"
+password="YOUR_PASSWORD_HERE"
+
+users_to_follow=[
+    "FIRST_USER_FOLLOWERS",
+    "OTHER_USER_FOLLOWERS"
+]
+
+white_list=[
+    "STILL_FOLLOW_THIS_USER",
+    "THIS_ONE_TOO"
+]
